08ee83b62c39a60ab04d78421b7cad1af60a9162
I3alu <gyorei.balazs@liveedu.mathiasz.hu>
2026-01-15 11:30:41 +0100

    haladtam a react-el

--- frontend/Temeto_nyilvantartas/src/App.jsx
+++ frontend/Temeto_nyilvantartas/src/App.jsx
@@ -2,10 +2,15 @@ import React from "react";
 import "./App.css";
 import MainPage from "./Pages/MainPage";
 import LoginPage from "./login_components/LoginPage";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <LoginPage />
-  //return <MainPage />;
+  return (
+    <Routes>
+      <Route path="/" element={<LoginPage />} />
+      <Route path="/main" element={<MainPage />} />
+    </Routes>
+  );
 }
 
 export default App;
